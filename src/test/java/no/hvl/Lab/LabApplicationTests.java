a30db521bae59ccf02bfceb9b82975750fb6a1c5
Thomas Tolo <thomastj278@gmail.com>
2025-09-22 16:58:53 +0200

    .

--- src/test/java/no/hvl/Lab/LabApplicationTests.java
+++ src/test/java/no/hvl/Lab/LabApplicationTests.java
@@ -2,10 +2,13 @@ package no.hvl.Lab;
 
 import org.junit.jupiter.api.Test;
 
+
+
 class LabApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+ 	@Test
+ 	void contextLoads() {
+ 		// Basic test, no Spring context
+ 	}
 
 }
